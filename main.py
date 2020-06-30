c12d96b7b3fb3966bea5f9395313d01c8929758c
Dinesh I <62064640+Dinesh-3@users.noreply.github.com>
2020-06-30 12:33:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import urllib.request
 import json
 
 channel_name = input("Enter Youtube channel name: ")
-key = "Enter your api key"
+key =  "AIzaSyDHVol6r4EEtiGe2rmUrTl-HndlpAHriWo" #Enter your api key
+try:
+    url = "https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername="+channel_name+"&key="+key
 
-url = "https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername="+name+"&key="+key
-
-data = urllib.request.urlopen(url).read()
-subcount = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
-
+    data = urllib.request.urlopen(url).read()
+    sub_count = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
+except:
+    print("Invalid channel name !!!")
 print(f"{channel_name} subcribers count is : {sub_count}")
+
+
